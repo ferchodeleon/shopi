b4b8fcffb43c0cc2468cb9db8846e3c46f866a0f
Fernando De Leon Gil <gasnare@hotmail.com>
2025-01-24 17:22:47 -0500

    Se agrega la API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from "react";
+import Card from "../../Components/Card";
 import Layout from "../../Components/Layout";
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <Layout>
-      <h1>Home</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {items && items.map((item) => <Card key={item.id} item={item} />)}
+      </div>
     </Layout>
   );
 };
