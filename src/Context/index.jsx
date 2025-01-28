928cf2761ce2188bf7ffba1af3cba93bb827069d
Fernando De Leon Gil <gasnare@hotmail.com>
2025-01-28 12:17:02 -0500

    Se exporta el context ShoppingCartContext y se agrega el count y setcount

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,11 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import PropTypes from "prop-types";
 
-const ShoppingCartContext = createContext();
+export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+  const [count, setCount] = useState(0);
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
