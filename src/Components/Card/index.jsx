9833d4d7f44f99dc792c0b5799d4d3cd3ae2e59c
Fernando De Leon Gil <gasnare@hotmail.com>
2025-01-28 14:26:52 -0500

    Added icon plus to add product shopping

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
+import { PlusIcon } from "@heroicons/react/24/solid";
 const Card = ({ item }) => {
   const [image, setImage] = useState(item.images[0]);
   const context = useContext(ShoppingCartContext);
@@ -28,7 +29,7 @@ const Card = ({ item }) => {
           className="absolute top-0 right-0 flex justify-center items-center bg-white text-black w-6 h-6 rounded-full m-2 p-1"
           onClick={() => context.setCount(context.count + 1)}
         >
-          +
+          <PlusIcon className="size-6 text-black" />
         </div>
         <span className="absolute bottom-0 left-0 flex bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
           {item.category.name}
