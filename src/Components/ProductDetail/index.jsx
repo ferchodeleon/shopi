c3f52cadebf067581d0453d74f590399e4c0761f
Fernando De Leon Gil <gasnare@hotmail.com>
2025-01-28 15:14:46 -0500

    Add logic to view productDetail and closed

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,13 +1,23 @@
+import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
+import { ShoppingCartContext } from "../../Context";
 import "./style.css";
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext);
   return (
-    <aside className="product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg">
+    <aside
+      className={`${
+        context.isProductDetailOpen ? "flex" : "hidden"
+      }  product-detail flex-col fixed bg-white right-0 border border-black rounded-lg`}
+    >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
-        <div>
+        <div
+          className="cursor-pointer"
+          onClick={() => context.closeProductDetail()}
+        >
           <XMarkIcon className="size-6 text-black" />
         </div>
       </div>
