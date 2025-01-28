75fe2784397dbe0ab69ae28ad54a563b28b2bc59
Fernando De Leon Gil <gasnare@hotmail.com>
2025-01-28 14:27:30 -0500

    Create component product detail

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,18 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+import "./style.css";
+
+const ProductDetail = () => {
+  return (
+    <aside className="product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg">
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">Detail</h2>
+        <div>
+          <XMarkIcon className="size-6 text-black" />
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default ProductDetail;
